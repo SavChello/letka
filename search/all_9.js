07b9217d2c939287210b1f074258b96b195470ec
SavChello <SavChello@users.noreply.github.com>
2026-08-28 14:12:10 +0000

    deploy: 07c0ca6412994347db2da64df6a54765c0a004f6

--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['neuroslop_0',['neuroslop',['../kvadr2_8cpp.html#a144a397c63f27640551a5fbcb511a223',1,'kvadr2.cpp']]],
-  ['new_5fread_5fnumber_1',['new_read_number',['../kvadr_8cpp.html#a25ebd0b35e6810ed308c1baf5d79ad16',1,'new_read_number(void):&#160;kvadr.cpp'],['../kvadr2_8cpp.html#af221ab9e21048816c1386d263e86eae0',1,'new_read_number(char argue):&#160;kvadr2.cpp'],['../kvadr3_8cpp.html#af221ab9e21048816c1386d263e86eae0',1,'new_read_number(char argue):&#160;kvadr3.cpp']]],
+  ['new_5fread_5fnumber_1',['new_read_number',['../kvadr3_8cpp.html#af221ab9e21048816c1386d263e86eae0',1,'kvadr3.cpp']]],
   ['no_5froots_2',['no_roots',['../kvadr_8cpp.html#a3d8f829519f9536e0f16e76614c5794dab9a321da349ac1355b717745736ad90c',1,'NO_ROOTS:&#160;kvadr.cpp'],['../kvadr2_8cpp.html#a3d8f829519f9536e0f16e76614c5794dab9a321da349ac1355b717745736ad90c',1,'NO_ROOTS:&#160;kvadr2.cpp'],['../kvadr3_8cpp.html#a3d8f829519f9536e0f16e76614c5794dab9a321da349ac1355b717745736ad90c',1,'NO_ROOTS:&#160;kvadr3.cpp']]]
 ];
